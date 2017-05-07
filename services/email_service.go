6c477e76b35bf28ab4b445723c8bee0933cbfced
ktabb <K128kevin@gmail.com>
2017-05-07 16:01:11 -0400

    parameterize email url

--- services/email_service.go
+++ services/email_service.go
@@ -13,7 +13,7 @@ func SendAccountVerificationEmail(username string, email string) {
 	fmt.Printf("\nToken: %s", token)
 	encoded := url.QueryEscape(token)
 	fmt.Printf("\nEncoded username: %s", encoded)
-	body := "<p><h3>Thank you for joining signing up with BTCPredictions! To verify your account and get started using the simulator, please click the link below.</h3></p><br><br><h4>http://localhost:4200/verify/" + encoded + "</h4>"
+	body := "<p><h3>Thank you for joining signing up with BTCPredictions! To verify your account and get started using the simulator, please click the link below.</h3></p><br><br><h4>http://" + os.Getenv("TRADESIM_HOST") + "/verify/" + encoded + "</h4>"
 	from := mail.NewEmail("BTCPredictions", "NoReply@BTCPredictions.com")
 	subject := "Please Verify BTCPredictions Account"
 	to := mail.NewEmail(username, email)
